f088c611a1ba200e09722bd493cef5b00aee4734
SebMD <39181223+sebmd@users.noreply.github.com>
2020-05-05 11:41:02 +0200

    2020-05-05-11:41:02

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echp "Pobieram vim-plug"
+echo "Pobieram vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Tworzę katalogi w ~/.vim/"
 mkdir -p ~/.vim/tmp/{backup,view,swap,viminfo}
 mkdir -p ~/.vim/colors
+
+echo "Instaluję pluginy"
+vim -c ":PlugInstall" -c ":q"
