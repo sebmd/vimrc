ae5a768584c135a38f709f405b7914159432ba2c
SebMD <39181223+sebmd@users.noreply.github.com>
2020-05-05 11:29:09 +0200

    2020-05-05-11:29:09

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # vimrc
+
+## Dodatkowe oprogramowanie
+
+- FZF
+- ripgrep
+- curl
+
+## Instalacja
+
+- plik `setup.sh`
+
+
+vim-plug https://github.com/junegunn/vim-plug
--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echp "Pobieram vim-plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo "Tworzę katalogi w ~/.vim/"
+mkdir -p ~/.vim/tmp/{backup,view,swap,viminfo}
+mkdir -p ~/.vim/colors
